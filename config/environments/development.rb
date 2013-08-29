55aca0ab54e02b48d47e1e8a221ccd28b09a8822
Jake Klingler <thearrow3456@gmail.com>
2013-08-29 17:21:18 -0400

    use loose assets in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,5 @@ Cleaneats::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.prefix = '/dev-assets'
 end
